4f4c23c5601f8626375196bc9311af67579d7860
saerhan <saeedbakhshmand@gmail.com>
2016-12-04 14:39:14 -0500

    Added capped off cylinder and clipped sphere

--- NeuroPath/NeuroPath.py
+++ NeuroPath/NeuroPath.py
@@ -12,16 +12,16 @@ renderWindow = slicer.app.layoutManager().threeDWidget(0).threeDView().renderWin
 renderWindow.AddRenderer(renderer)
 renderWindowInteractor = slicer.app.layoutManager().threeDWidget(0).threeDView().interactor()
 renderWindowInteractor.SetRenderWindow(renderWindow)
-
+ 
 #
 # NeuroPath
 #
-
+ 
 class NeuroPath(ScriptedLoadableModule):
   """Uses ScriptedLoadableModule base class, available at:
   https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
   """
-
+ 
   def __init__(self, parent):
     ScriptedLoadableModule.__init__(self, parent)
     self.parent.title = "NeuroPath" # TODO make this more human readable by adding spaces
@@ -36,28 +36,28 @@ class NeuroPath(ScriptedLoadableModule):
     This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
     and Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
 """ # replace with organization, grant and thanks.
-
+ 
 #
 # NeuroPathWidget
 #
-
+ 
 class NeuroPathWidget(ScriptedLoadableModuleWidget):
   """Uses ScriptedLoadableModuleWidget base class, available at:
   https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
   """
-
+ 
   def setup(self):
     ScriptedLoadableModuleWidget.setup(self)
-
+ 
     # Instantiate and connect widgets ...
-
+ 
     #
     # Parameters Area
     #
     parametersCollapsibleButton = ctk.ctkCollapsibleButton()
     parametersCollapsibleButton.text = "Parameters"
     self.layout.addWidget(parametersCollapsibleButton)
-
+ 
     # Layout within the dummy collapsible button
     parametersFormLayout = qt.QFormLayout(parametersCollapsibleButton)
     #
@@ -75,7 +75,7 @@ class NeuroPathWidget(ScriptedLoadableModuleWidget):
     self.ROISelector.setToolTip( "Pick the ROI Interactive Box to define the region for labeling of Fibers." )
     parametersFormLayout.addRow("Fiducial : ", self.ROISelector)
     self.ROISelector.connect("currentNodeChanged(vtkMRMLNode*)", self.OnSelect_ROI)
-
+ 
     #
     # Sphere value
     #
@@ -87,8 +87,8 @@ class NeuroPathWidget(ScriptedLoadableModuleWidget):
     self.SliderSphere.setToolTip("Set threshold value for computing the output image. Voxels that have intensities lower than this value will set to zero.")
     self.SliderSphere.valueChanged.connect(self.onSliderChange)
     parametersFormLayout.addRow("Sphere Radius", self.SliderSphere)
-	
-	#
+     
+    #
     # Cylinder value
     #
     self.SliderCylinder = ctk.ctkSliderWidget()
@@ -99,9 +99,9 @@ class NeuroPathWidget(ScriptedLoadableModuleWidget):
     self.SliderCylinder.setToolTip("Set threshold value for computing the output image. Voxels that have intensities lower than this value will set to zero.")
     self.SliderCylinder.valueChanged.connect(self.onSliderChange)
     parametersFormLayout.addRow("Cylinder Radius", self.SliderCylinder)
-	
-	
-	#
+     
+     
+    #
     # Cylinder value
     #
     self.SliderCylinder_2 = ctk.ctkSliderWidget()
@@ -112,7 +112,7 @@ class NeuroPathWidget(ScriptedLoadableModuleWidget):
     self.SliderCylinder_2.setToolTip("Set threshold value for computing the output image. Voxels that have intensities lower than this value will set to zero.")
     self.SliderCylinder_2.valueChanged.connect(self.onSliderChange)
     parametersFormLayout.addRow("Cylinder Height", self.SliderCylinder_2)
-	#
+    #
     # Apply Button
     #
     self.applyButton_7 = qt.QCheckBox("Update the graphics")
@@ -121,10 +121,10 @@ class NeuroPathWidget(ScriptedLoadableModuleWidget):
     parametersFormLayout.addRow("Image threshold", self.applyButton_7)
     # connections
     self.applyButton_7.stateChanged.connect(self.onApplyButton_7)
-	
-	
+     
+     
   def OnSelect_ROI(self):
-      global ftone,fttwo,ft12,fss,markupNode,sphere,cylinder,logic,Actor2
+      global ftone,fttwo,ft12,fss,markupNode,sphere,cylinder,logic,Actor2, plane, actor3
       global ft12
       logic = NeuroPathLogic()
       ftone = [0,0,0]
@@ -140,18 +140,33 @@ class NeuroPathWidget(ScriptedLoadableModuleWidget):
       Actor = vtk.vtkActor()
       Actor.SetMapper(Mapper)
       Actor.GetProperty().SetColor(0,1,0)
-      Actor.GetProperty().SetOpacity(0.3)
-      renderer.AddActor(Actor)
+      #Actor.GetProperty().SetOpacity(0.9)
+      #renderer.AddActor(Actor)
+      clip = vtk.vtkClipPolyData()
+      clip.SetValue(0);
+      clip.GenerateClippedOutputOn();
+      clip.SetInputConnection(sphere.GetOutputPort())
+      plane = vtk.vtkPlane()
+      plane.SetNormal(0.0, 1.0, 0.0)
+      plane.SetOrigin(0.0, 10.0, 0.0)
+      clip.SetClipFunction(plane)
+      polyDataMapper = vtk.vtkPolyDataMapper()
+      polyDataMapper.SetInputConnection(clip.GetOutputPort()) 
+      actor3 = vtk.vtkActor()
+      actor3.SetMapper(polyDataMapper)
+      actor3.GetProperty().SetColor(0,1,0)
+      renderer.AddActor(actor3)
       cylinder = vtk.vtkCylinderSource()
       cylinder.SetRadius(5)
       cylinder.SetHeight(10)
       cylinder.SetResolution(100)
+      cylinder.CappingOff()
       Mapper2 = vtk.vtkPolyDataMapper()
       Mapper2.SetInputConnection(cylinder.GetOutputPort())
       Actor2 = vtk.vtkActor()
       Actor2.SetMapper(Mapper2)
       Actor2.GetProperty().SetColor(0,0,1)
-      Actor2.GetProperty().SetOpacity(0.3)
+      #Actor2.GetProperty().SetOpacity(0.9)
       renderer.AddActor(Actor2)
       Transform = vtk.vtkTransform()
       Transform.PostMultiply()
@@ -186,9 +201,9 @@ class NeuroPathWidget(ScriptedLoadableModuleWidget):
     logic.run_7()
   def onSelect(self):
     self.applyButton.enabled = self.inputSelector.currentNode() and self.outputSelector.currentNode()
-
+ 
   def onApplyButton(self):
-    
+     
     enableScreenshotsFlag = self.enableScreenshotsFlagCheckBox.checked
     imageThreshold = self.imageThresholdSliderWidget.value
     logic.run(self.inputSelector.currentNode(), self.outputSelector.currentNode(), imageThreshold, enableScreenshotsFlag)
@@ -198,7 +213,7 @@ class NeuroPathWidget(ScriptedLoadableModuleWidget):
 #
 # NeuroPathLogic
 #
-
+ 
 class NeuroPathLogic(ScriptedLoadableModuleLogic):
   """This class should implement all the actual
   computation done by your module.  The interface
@@ -208,7 +223,7 @@ class NeuroPathLogic(ScriptedLoadableModuleLogic):
   Uses ScriptedLoadableModuleLogic base class, available at:
   https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
   """
-
+ 
   def run_7(self):#Run the computation
     ft1 = np.array(ftone)
     ft2 = np.array(fttwo)
@@ -223,6 +238,8 @@ class NeuroPathLogic(ScriptedLoadableModuleLogic):
     C_Cent = ((ft1+ft2)/2)
     cylinder.SetCenter(C_Cent)
     ft12  = ft1-ft2
+    plane.SetNormal(ft12)
+    plane.SetOrigin(ft1)
     Y_axis = np.array([0,1,0])
     sphere.SetCenter(fs)
     cylinder.SetRadius(R_cylinder)
@@ -235,32 +252,32 @@ class NeuroPathLogic(ScriptedLoadableModuleLogic):
     Transform.RotateWXYZ(-1*np.degrees(logic.py_ang(ft12,Y_axis)),np.cross(ft12,Y_axis))
     Transform.Translate(C_Cent[0],C_Cent[1],C_Cent[2])
     print(cylinder.GetCenter())
-
+ 
   def py_ang(self,v1, v2):
    """ Returns the angle in radians between vectors 'v1' and 'v2'    """
    cosang = np.dot(v1, v2)
    sinang = la.norm(np.cross(v1, v2))
    return np.arctan2(sinang, cosang)
-
-
+ 
+ 
 class NeuroPathTest(ScriptedLoadableModuleTest):
   """
   This is the test case for your scripted module.
   Uses ScriptedLoadableModuleTest base class, available at:
   https://github.com/Slicer/Slicer/blob/master/Base/Python/slicer/ScriptedLoadableModule.py
   """
-
+ 
   def setUp(self):
     """ Do whatever is needed to reset the state - typically a scene clear will be enough.
     """
     slicer.mrmlScene.Clear(0)
-
+ 
   def runTest(self):
     """Run as few or as many tests as needed here.
     """
     self.setUp()
     self.test_NeuroPath1()
-
+ 
   def test_NeuroPath1(self):
     """ Ideally you should have several levels of tests.  At the lowest level
     tests should exercise the functionality of the logic with different inputs
@@ -272,7 +289,7 @@ class NeuroPathTest(ScriptedLoadableModuleTest):
     module.  For example, if a developer removes a feature that you depend on,
     your test should break so they know that the feature is needed.
     """
-
+ 
     self.delayDisplay("Starting the test")
     #
     # first, get some data
@@ -281,7 +298,7 @@ class NeuroPathTest(ScriptedLoadableModuleTest):
     downloads = (
         ('http://slicer.kitware.com/midas3/download?items=5767', 'FA.nrrd', slicer.util.loadVolume),
         )
-
+ 
     for url,name,loader in downloads:
       filePath = slicer.app.temporaryPath + '/' + name
       if not os.path.exists(filePath) or os.stat(filePath).st_size == 0:
@@ -291,7 +308,7 @@ class NeuroPathTest(ScriptedLoadableModuleTest):
         logging.info('Loading %s...' % (name,))
         loader(filePath)
     self.delayDisplay('Finished with download and loading')
-
+ 
     volumeNode = slicer.util.getNode(pattern="FA")
     logic = NeuroPathLogic()
     self.assertTrue( logic.hasImageData(volumeNode) )
